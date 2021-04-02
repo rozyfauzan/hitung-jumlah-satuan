1e05c8aa7fe774062f8ed8b095b7af7856236222
rozyfauzan <rozyfauzan28@gmail.com>
2021-04-02 09:41:19 +0700

    Menambahkan Fragment Rumus Aplikasi +  Tentang Aplikasi

---
+++ app/src/main/java/org/d3if4079/hitungjumlahsatuan/ui/AboutFragment.kt
@@ -0,0 +1,7 @@
+package org.d3if4079.hitungjumlahsatuan.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if4079.hitungjumlahsatuan.R
+
+
+class AboutFragment : Fragment(R.layout.fragment_about)
---
+++ app/src/main/java/org/d3if4079/hitungjumlahsatuan/ui/RumusFragment.kt
@@ -0,0 +1,6 @@
+package org.d3if4079.hitungjumlahsatuan.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if4079.hitungjumlahsatuan.R
+
+class RumusFragment :  Fragment (R.layout.fragment_rumus);
